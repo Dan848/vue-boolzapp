fe6b95b3c821e45ad0e78d60974b495321696738
Dan848 <daniele.ceravolo15@gmail.com>
2023-04-04 18:40:26 +0200

    add date to single message in chat

--- js/script.js
+++ js/script.js
@@ -11,17 +11,17 @@ createApp({
                     state: "offline",
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2020  15:30:55',
                             message: 'Hai portato a spasso il cane?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2020  15:50:00',
                             message: 'Ricordati di stendere i panni',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 16:15:22',
+                            date: '10/01/2020  16:15:22',
                             message: 'Tutto fatto!',
                             status: 'received'
                         }
@@ -34,17 +34,17 @@ createApp({
                     state: "offline",
                     messages: [
                         {
-                            date: '20/03/2020 16:30:00',
+                            date: '20/03/2020  16:30:00',
                             message: 'Ciao come stai?',
                             status: 'sent'
                         },
                         {
-                            date: '20/03/2020 16:30:55',
+                            date: '20/03/2020  16:30:55',
                             message: 'Bene grazie! Stasera ci vediamo?',
                             status: 'received'
                         },
                         {
-                            date: '20/03/2020 16:35:00',
+                            date: '20/03/2020  16:35:00',
                             message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                             status: 'sent'
                         }
@@ -57,17 +57,17 @@ createApp({
                     state: "offline",
                     messages: [
                         {
-                            date: '28/03/2020 10:10:40',
+                            date: '28/03/2020  10:10:40',
                             message: 'La Marianna va in campagna',
                             status: 'received'
                         },
                         {
-                            date: '28/03/2020 10:20:10',
+                            date: '28/03/2020  10:20:10',
                             message: 'Sicuro di non aver sbagliato chat?',
                             status: 'sent'
                         },
                         {
-                            date: '28/03/2020 16:15:22',
+                            date: '28/03/2020  16:15:22',
                             message: 'Ah scusa!',
                             status: 'received'
                         }
@@ -80,12 +80,12 @@ createApp({
                     state: "offline",
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2020  15:30:55',
                             message: 'Lo sai che ha aperto una nuova pizzeria?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2020  15:50:00',
                             message: 'Si, ma preferirei andare al cinema',
                             status: 'received'
                         }
@@ -98,12 +98,12 @@ createApp({
                     state: "offline",
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2020  15:30:55',
                             message: 'Ricordati di chiamare la nonna',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2020  15:50:00',
                             message: 'Va bene, stasera la sento',
                             status: 'received'
                         }
@@ -116,17 +116,17 @@ createApp({
                     state: "offline",
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2020  15:30:55',
                             message: 'Ciao Claudia, hai novità?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2020  15:50:00',
                             message: 'Non ancora',
                             status: 'received'
                         },
                         {
-                            date: '10/01/2020 15:51:00',
+                            date: '10/01/2020  15:51:00',
                             message: 'Nessuna nuova, buona nuova',
                             status: 'sent'
                         }
@@ -139,12 +139,12 @@ createApp({
                     state: "offline",
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2020  15:30:55',
                             message: 'Fai gli auguri a Martina che è il suo compleanno!',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2020  15:50:00',
                             message: 'Grazie per avermelo ricordato, le scrivo subito!',
                             status: 'received'
                         }
@@ -157,17 +157,17 @@ createApp({
                     state: "offline",
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2020  15:30:55',
                             message: 'Ciao, andiamo a mangiare la pizza stasera?',
                             status: 'received'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2020  15:50:00',
                             message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:51:00',
+                            date: '10/01/2020  15:51:00',
                             message: 'OK!!',
                             status: 'received'
                         }
@@ -271,12 +271,14 @@ createApp({
             this.moveUpChat(this.contacts, this.findIndexToId(this.contacts, this.contId))
             //Clear input box
             this.messToSendText = "";
-            //Writing status true
+            //Change Status to Writing
             this.contacts[this.findIndexToId(this.contacts, this.contId)].state = "writing";
             //Taking current id so it doesn't change if we click on another contact
             const actualId = this.contId;
             setTimeout(() =>{
+                //Change Status to Online                
                 this.contacts[this.findIndexToId(this.contacts, actualId)].state = "online";
+                //Push answer back
                 this.contacts[this.findIndexToId(this.contacts, actualId)].messages.push(
                     {
                         date: this.getDate(),
@@ -286,6 +288,7 @@ createApp({
                     
                 );
             },3000)
+            //Change Status to Offline            
             setTimeout(() => this.contacts[this.findIndexToId(this.contacts, actualId)].state = "offline", 6000);
         }
     },
